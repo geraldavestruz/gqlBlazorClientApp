5aec773c8b6bb5838d91fdb8c2946fb754f87033
gerald <geraldavestruz96@gmail.com>
2022-05-04 09:46:34 +0800

    add mods

---
+++ Dtos/CakeDtos.cs
@@ -0,0 +1,9 @@
+namespace ClientApp.Dtos
+{
+    public class CakeDtos
+    {
+        public int Id { get; set; }
+        
+        public string Flavor { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddcakeGqlClient().ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:7156/graphql/"));
 
 await builder.Build().RunAsync();
